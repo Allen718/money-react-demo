49b3a9bb5d5d3c4057117203699534ecce8cad47
Administrator <yuy20170301@126.com>
2020-08-31 11:56:32 +0800

    change url

---
+++ precache-manifest.4b4143e8b1d905401fef1070efdb9d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631057c8d7a3fbb07186b4277ea95e75",
+    "url": "/money-react-demo/index.html"
+  },
+  {
+    "revision": "377e068bab78e927c162",
+    "url": "/money-react-demo/static/css/main.127124cc.chunk.css"
+  },
+  {
+    "revision": "1aaf36488758338c2929",
+    "url": "/money-react-demo/static/js/2.22e8b61f.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/money-react-demo/static/js/2.22e8b61f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377e068bab78e927c162",
+    "url": "/money-react-demo/static/js/main.b811ffbb.chunk.js"
+  },
+  {
+    "revision": "66dcc1c63c48c2b40a00",
+    "url": "/money-react-demo/static/js/runtime-main.fc1dd156.js"
+  }
+]);
